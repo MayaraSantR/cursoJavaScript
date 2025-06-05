11714915bd300713ad397e7c59af46b4bff102bd
Mayara <mayarasantar@gmail.com>
2025-06-04 21:49:26 -0300

    Aula04

---
+++ aula04.06/exer.js
@@ -0,0 +1,11 @@
+let mayara = document.getElementById("mayara")
+
+mayara.style.fontSize = "60px"
+mayara.style.backgroundColor = "blue"
+
+let button = document.getElementById("button")
+
+button.addEventListener("click", function(){
+    titulo.innerText = "Clique no Botão"
+    titulo.style.color = "black"
+ })
---
+++ aula04.06/script.js
@@ -0,0 +1,41 @@
+
+// let titulo = document.getElementById("titulo")
+// console.log(titulo)
+
+// let paragrafo = document.getElementById("paragrafo")
+// console.log(paragrafo)
+
+// let paragrafo2 = document.getElementById("paragrafo2")
+// console.log(paragrafo2)
+
+// let pedro = document.getElementById("pedro")
+// console.log(pedro)
+
+// let img = document.getElementById("img")
+// console.log(img)
+
+// let button = document.getElementById("button")
+// console.log(button)
+
+//-----------Usando Class ----------------
+
+let p1 = document.getElementsByClassName("p1")
+console.log(p1)
+
+let texto = document.getElementsByClassName("texto")
+console.log(texto)
+
+
+let titulo = document.getElementById("titulo")
+
+ titulo.style.color = "pink"
+ titulo.style.fontSize = "50px"
+ titulo.style.backgroundColor  = "black"
+ titulo.style.border = "10px solid black"
+
+let button = document.getElementById("button")
+
+ button.addEventListener("click", function(){
+    titulo.innerText = "Clique no Botão"
+    titulo.style.color = "purple"
+ })
